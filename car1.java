d0931da9deae087b2faf530c4284f9ded216cb98
saidivya123442 <saidivyayanamala17@gmail.com>
2025-12-01 21:53:40 +0530

    Create car1.java

---
+++ car1.java
@@ -0,0 +1,29 @@
+class Car1 {
+    String model;
+    int year;
+
+    // Parameterized Constructor
+    Car1(String m, int y) {
+        model = m;
+        year = y;
+    }
+
+    // Copy Constructor
+    Car1(Car1 c) {
+        model = c.model;
+        year = c.year;
+    }
+
+    void display() {
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public static void main(String[] args) {
+        Car1 c1 = new Car1("Honda City", 2022);
+        Car1 c2 = new Car1(c1); 
+
+        c1.display();
+        c2.display();
+    }
+}
